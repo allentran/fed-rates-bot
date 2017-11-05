1daf5d61e3e18ca2f892193a6a9b8e701ca293e3
Allen Tran <realallentran@gmail.com>
2017-11-04 17:02:08 -0700

    use GloVe vocab, rm Spacy for NLTK

--- fed_bot/model/data.py
+++ fed_bot/model/data.py
@@ -5,14 +5,12 @@ import os
 import re
 import datetime
 
+import nltk
 import unidecode
-from spacy.en import English
 import requests
 import pandas as pd
-import numpy as np
-import allen_utils
+import boto3
 
-logger = allen_utils.get_logger(__name__)
 
 class Interval(object):
 
@@ -36,12 +34,14 @@ fed_regimes = {
     5: Interval(datetime.date(2006, 2, 1), datetime.date(2020, 1, 31)),
 }
 
+
 def find_regime(date):
 
     for regime, interval in fed_regimes.iteritems():
         if interval.contains(date):
             return regime
     raise ValueError("Could not find regime for date, %s", date)
+
 
 class PairedDocAndRates(object):
 
@@ -53,7 +53,7 @@ class PairedDocAndRates(object):
         self.rates = None
         self.regime = find_regime(date)
 
-    def match_rates(self, rates_df, days = [30, 90, 180]):
+    def match_rates(self, rates_df, days = (30, 90, 180, 365)):
 
         def get_closest_rate(days_to_add):
             future_date = self.date + datetime.timedelta(days=days_to_add)
@@ -86,6 +86,7 @@ class PairedDocAndRates(object):
             regime = self.regime
         )
 
+
 class Vocab(object):
 
     def __init__(self):
@@ -115,6 +116,14 @@ class Vocab(object):
 
         return position_dict
 
+
+def get_json_s3(bucket, key):
+    def get_object(bucket, key):
+        s3 = boto3.resource('s3')
+        return s3.meta.client.get_object(Bucket=bucket, Key=key)['Body'].read()
+    return json.loads(get_object(bucket, key))
+
+
 class DataTransformer(object):
 
     def __init__(self, data_dir, min_sentence_length):
@@ -127,16 +136,7 @@ class DataTransformer(object):
             'CARDINAL': '$CARDINAL$'
         }
 
-
-        self.nlp = English()
-
-        # custom token replacement
-        self.regexes = [
-            (re.compile(r'\d{4}'), '$DATE$'),
-            (re.compile(r'\d+[\.,]*\d+'), '$CARDINAL$')
-        ]
-
-        self.vocab = Vocab()
+        self.vocab = get_json_s3('litidata', 'misc/glove.vocab.json')
         self.word_positions = None
 
         self.rates = None
@@ -153,45 +153,7 @@ class DataTransformer(object):
         if r.status_code == 200:
             self.rates = pd.DataFrame(r.json()['observations'])
             self.rates['date'] = self.rates['date'].apply(lambda s: datetime.datetime.strptime(s, '%Y-%m-%d').date())
-            self.rates.sort('date')
-
-    def build_vocab(self):
-
-        def process_doc(doc_path):
-            with open(doc_path, 'r') as f:
-                text = unidecode.unidecode(unicode(f.read().decode('iso-8859-1')))
-                text = ' '.join(text.split()).strip()
-            if len(text) > 0:
-                doc = self.nlp(unicode(text.lower()))
-                doc_words = set()
-                for sent in doc.sents:
-                    if len(sent) > self.min_sentence_length:
-                        for token in doc:
-                            if token.text not in doc_words:
-                                self.vocab.update_count(token.text)
-                                doc_words.add(token.text)
-
-        file_re = re.compile(r'\d{8}')
-
-        for root, dirs, filenames in os.walk(self.data_dir):
-            for filename in filenames:
-                if file_re.search(filename):
-                    filepath = os.path.join(root, filename)
-                    process_doc(filepath)
-                    logger.info("Built vocab from: %s", filepath)
-
-        self.word_positions = self.vocab.to_dict()
-
-    def strip_text(self, text):
-
-        doc = self.nlp(unicode(text).lower())
-
-        # spacy entity replacement
-        ents_dict = {ent.text: self.replace_entities[ent.label_] for ent in doc.ents if ent.label_ in self.replace_entities.keys()}
-        for ent in ents_dict:
-            text = text.replace(ent, ents_dict[ent])
-
-        return text
+            self.rates.sort_values(by='date')
 
     def get_docs(self, min_sentence_length=8):
 
@@ -201,27 +163,18 @@ class DataTransformer(object):
                 text = ' '.join(text.split()).strip()
             if len(text) > 0:
                 date = datetime.datetime.strptime(date_re.search(doc_path).group(0), '%Y%m%d').date()
-                stripped_text = self.strip_text(text)
 
-                doc = self.nlp(unicode(stripped_text))
-                sentences = list(doc.sents)
+                sentences = nltk.sent_tokenize(text.lower())
                 doc_sents = []
 
                 for sent in sentences[1:]:
                     if len(sent) > min_sentence_length:
                         sentence_as_idxes = []
-                        for token in sent:
-                            skip = False
-                            for regex, replacement_token in self.regexes:
-                                match = regex.match(token.text)
-                                if match:
-                                    sentence_as_idxes.append(self.word_positions[replacement_token])
-                                    skip = True
-                            if not skip:
-                                try:
-                                    sentence_as_idxes.append(self.word_positions[token.text])
-                                except KeyError:
-                                    sentence_as_idxes.append(self.word_positions['$UNKNOWN$'])
+                        for token in nltk.word_tokenize(sent):
+                            if token in self.vocab:
+                                sentence_as_idxes.append(self.vocab[token])
+                            else:
+                                sentence_as_idxes.append(0)
                         doc_sents.append(sentence_as_idxes)
 
                 paired_doc = PairedDocAndRates(date, doc_sents, doc_path.find('minutes') > -1)
@@ -239,7 +192,6 @@ class DataTransformer(object):
                     filepath = os.path.join(root, filename)
                     parsed_doc = parse_doc(filepath)
                     if parsed_doc:
-                        logger.info("Parsed %s", filepath)
                         docs.append(parsed_doc)
 
         self.docs = docs
@@ -250,12 +202,11 @@ class DataTransformer(object):
             json.dump([doc.to_dict() for doc in self.docs], f, indent=2, sort_keys=True)
 
         with open(os.path.join(self.data_dir, 'dictionary.json'), 'w') as f:
-            json.dump(self.vocab.to_dict(), f, indent=2, sort_keys=True)
+            json.dump(self.vocab, f, indent=2, sort_keys=True)
 
 if __name__ == "__main__":
 
     data_transformer = DataTransformer('data', min_sentence_length=8)
-    data_transformer.build_vocab()
     data_transformer.get_rates('51c09c6b8aa464671aa8ac96c76a8416')
     data_transformer.get_docs()
     data_transformer.save_output()
